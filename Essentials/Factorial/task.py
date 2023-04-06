3c6f979a7bb336180a7dbe5e33a23ef3e62cf8d1
Volodymyr Pabyrivskyi <pabyrivskyi@gmail.com>
2023-04-07 00:03:18 +0300

    Implement factorial tail recursion.

--- Essentials/Factorial/task.py
+++ Essentials/Factorial/task.py
@@ -2,6 +2,14 @@ from typing import Callable
 
 from Essentials.Factorial.tail_recursion import tail_call_optimized
 
-
 def factorial_impl() -> Callable[[int], int]:
-    raise Exception('Implement me')
+
+    @tail_call_optimized     
+    def factorial_tail_recursion(x: int, acc: int = 1):
+        if(x <= 1):
+            return acc
+
+        return factorial_tail_recursion(x - 1, acc * x)
+
+    return factorial_tail_recursion
+    
