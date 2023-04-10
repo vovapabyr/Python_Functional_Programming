3924051f4f871f7a71d25e00c55b897369e977e7
Volodymyr Pabyrivskyi <pabyrivskyi@gmail.com>
2023-04-10 17:11:25 +0300

    Implement fibonacci tail recursion.

--- Essentials/TailFibonacci/task.py
+++ Essentials/TailFibonacci/task.py
@@ -4,4 +4,13 @@ from Essentials.TailFibonacci.tail_recursion import tail_call_optimized
 
 
 def fibonacci_impl() -> Callable[[int], int]:
-    raise Exception('Implement me')
+    
+    @tail_call_optimized
+    def fibonacci_tail_recursion(n: int, index: int = 0, previous: int = 0, next: int = 1):
+        if index == n:
+            return previous
+        
+        return fibonacci_tail_recursion(n, index + 1, next, previous + next)
+    
+    return fibonacci_tail_recursion
+
