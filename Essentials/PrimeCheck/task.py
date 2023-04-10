7ce31853d39a8cc3a0df0a933783d1860ffa3ba4
Volodymyr Pabyrivskyi <pabyrivskyi@gmail.com>
2023-04-10 13:51:08 +0300

    Implement prime number check.

--- Essentials/PrimeCheck/task.py
+++ Essentials/PrimeCheck/task.py
@@ -1,2 +1,16 @@
+from math import ceil, sqrt
+
+
 def is_prime(n: int) -> bool:
-    raise Exception('Implement me')
+    if (n < 2):
+        return False
+
+
+    def is_prime_int(factor: int):
+        if(factor == 1):
+            return True
+        
+        return n % factor != 0 and is_prime_int(factor - 1) 
+
+    # Check till sqrt(n) as a larger factor of n must be a multiple of a smaller factor that has been already checked
+    return is_prime_int(ceil(sqrt(n)))
