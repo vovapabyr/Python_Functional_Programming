8a9157abf111519d0c785a643629578342b3dde9
Volodymyr Pabyrivskyi <pabyrivskyi@gmail.com>
2023-04-10 13:08:59 +0300

    Implement fibonacci.

--- Essentials/Fibonacci/task.py
+++ Essentials/Fibonacci/task.py
@@ -2,4 +2,11 @@ from typing import Callable
 
 
 def fibonacci_impl() -> Callable[[int], int]:
-    raise Exception("Implement me")
+
+    def fibonacci(x: int):
+         if (x < 2):
+            return x
+         
+         return fibonacci(x - 1) + fibonacci(x - 2)
+    
+    return fibonacci
